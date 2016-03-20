ac920d95cb07409c9ff6e9b156ccec94ab0df0f1
gorranian <gorranian@yahoo.com.br>
2016-03-19 21:01:52 -0300

    More progress + about me

--- Portfolio_Memoria/public/javascripts/main.js
+++ Portfolio_Memoria/public/javascripts/main.js
@@ -1,4 +1,20 @@
-﻿
-$(document).ready(function () {
+﻿var lockpos = $(window).height()*0.7;
 
+$(document).ready(function () {
+    $(window).on("resize", function (e) {
+        var lockpos = $(window).height()*0.7;
+        lockName();
+    });
+
+    $(window).on("scroll", function (e) {
+        lockName();
+    });
 });
+
+function lockName(){
+    if ($(window).scrollTop() > lockpos) {
+        $(".namediv").addClass("fixedname");
+    } else {
+        $(".namediv").removeClass("fixedname");
+    }
+}
