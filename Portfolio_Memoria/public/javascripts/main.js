693d9ba48ff96428139e5ec274ab106776205b00
gorranian <gorranian@yahoo.com.br>
2016-04-16 13:29:15 -0300

    Fixed with image slider

--- Portfolio_Memoria/public/javascripts/main.js
+++ Portfolio_Memoria/public/javascripts/main.js
@@ -1,5 +1,6 @@
 ﻿var lockpos = $(window).height() * 0.7;
 var bannerlist = ['/images/banner0_portfolio_yuri.jpg', '/images/banner1_portfolio_yuri.jpg', '/images/banner2_portfolio_yuri.jpg']
+var active_slideshow = "none"
 
 $(document).ready(function () {
     $(window).on("resize", function (e) {
@@ -19,20 +20,44 @@ $(document).ready(function () {
         bannerindex++;
         bannerindex = bannerindex % bannerlist.length;
     }, 7000);
-    
-    $("#aboutme_slide").slideme({
-        arrows: true,
-        pagination: "numbers",
-        resizable: {
-            width: 990,
-            height: 450,
-        }
-    });
 
     //ON CLICK events
 
     $("#aboutme_click").on("click", function () {
-        $(".blackoverlay").fadeIn();
+        $(".blackoverlay").fadeIn(1000);
+        $("#aboutme_slide").fadeIn(1000);
+        active_slideshow = "#aboutme_slide";
+    });
+
+    $(".arrowright").on("click", function () {
+        var picslist = $(active_slideshow + " ul > li");
+        var itemFadeIn;
+        picslist.each(function (index) {
+            if ($(this).is(":visible")) {
+                $(this).fadeOut();
+                if (index+1 == picslist.length) {
+                    itemFadeIn = 0;
+                } else {
+                    itemFadeIn = index + 1;
+                }
+            }
+        });
+        $(picslist[itemFadeIn]).fadeIn();
+    });
+    $(".arrowleft").on("click", function () {
+        var picslist = $(active_slideshow + " ul > li");
+        var itemFadeIn;
+        picslist.each(function (index) {
+            if ($(this).is(":visible")) {
+                $(this).fadeOut();
+                if (index - 1 < 0) {
+                    itemFadeIn = picslist.length-1;
+                } else {
+                    itemFadeIn = index - 1;
+                }
+            }
+        });
+        $(picslist[itemFadeIn]).fadeIn();
     });
 });
 
