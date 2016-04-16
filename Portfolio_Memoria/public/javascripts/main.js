693cf83442f6af4bb2199dc5ff5ea1996c532f19
gorranian <gorranian@yahoo.com.br>
2016-04-15 21:40:00 -0300

    unstable update with image slide and link button issue

--- Portfolio_Memoria/public/javascripts/main.js
+++ Portfolio_Memoria/public/javascripts/main.js
@@ -1,4 +1,5 @@
-﻿var lockpos = $(window).height()*0.7;
+﻿var lockpos = $(window).height() * 0.7;
+var bannerlist = ['/images/banner0_portfolio_yuri.jpg', '/images/banner1_portfolio_yuri.jpg', '/images/banner2_portfolio_yuri.jpg']
 
 $(document).ready(function () {
     $(window).on("resize", function (e) {
@@ -8,6 +9,30 @@ $(document).ready(function () {
 
     $(window).on("scroll", function (e) {
         lockName();
+    });
+    var bannerindex = 0;
+    setInterval(function () {
+        $(".pagetop").animate({ opacity: 0 }, 200, function () {
+            $(".pagetop").css('background-image', "url('/images/banner" + bannerindex + "_portfolio_yuri.jpg')");
+            $(".pagetop").animate({ opacity: 1 }, 200);
+        });
+        bannerindex++;
+        bannerindex = bannerindex % bannerlist.length;
+    }, 7000);
+    
+    $("#aboutme_slide").slideme({
+        arrows: true,
+        pagination: "numbers",
+        resizable: {
+            width: 990,
+            height: 450,
+        }
+    });
+
+    //ON CLICK events
+
+    $("#aboutme_click").on("click", function () {
+        $(".blackoverlay").fadeIn();
     });
 });
 
